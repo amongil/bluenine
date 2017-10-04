4eb5a1df5fe6f00309bedba79cb57fb31e486c20
amongil <hello@alvaromongil.com>
2017-10-04 17:56:07 +0200

    Add minimal clap CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+ 
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    match matches.subcommand_name() {
+        Some("create") => println!("create!"),
+        Some("show") => println!("show!"),
+        Some("refresh") => println!("delete!"),
+        Some("clean") => println!("clean!"),
+        _ => println!("You must specify something")
+    }
 }
