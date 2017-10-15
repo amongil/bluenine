6d9dfbd9941f9015005b87d7d3673ebe20062ecd
amongil <hello@alvaromongil.com>
2017-10-15 17:45:15 +0200

    Refactor app

--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,6 @@ pub mod SessionHandler {
     use rusoto_sts::{Sts, StsClient, GetSessionTokenRequest, GetSessionTokenResponse, GetSessionTokenError, Credentials};
     use std::collections::HashMap;
 
-    pub struct SessionHandler {
-        AWSConfig: AWSConfig,
-    }
-
     struct AWSConfig {
         profiles: HashMap<String, AWSProfile>,
     }
@@ -42,141 +38,56 @@ pub mod SessionHandler {
         }
     }
 
-    impl SessionHandler {
-        pub fn load_config(&mut self) {
-            let aws_config_file = read_aws_config_file();
-            let profiles = split_config_file(aws_config_file);
-
-            // Create an instance of the AWSConfig struct
-            let mut aws_config = AWSConfig {
-                profiles: HashMap::new()
-            };
-
-            // Iterate over profile chunks
-            for profile in profiles {
-                // Create a vector of string that holds each line
-                let split = profile.split("\n");
-                let lines: Vec<String> = split.map(|s| s.to_string()).collect();
-
-                // Get the profile name from the first line
-                let profile_line = lines[0].to_owned();
-
-                // Lets ignore the default for now
-                if profile_line == "[default]" {
-                    continue;
-                }
-
-                let split2 = profile_line.split(" ");
-                let mut words: Vec<String> = split2.map(|s| s.to_string()).collect();
-                let mut profile_name = String::new();
-                if words.len() > 1 {
-                    words[1].pop();
-                    profile_name = words[1].trim_right().to_owned();
-                }
-
-                let mut aws_profile = AWSProfile{
-                    source_profile: None,
-                    region: None,
-                    output: None,
-                    role_arn: None,
-                    mfa_serial: None,
-                    ot_expiration: None,
-                    ot_session_name: None,
-                    ot_source_profile: None,
-                    ot_role_arn: None,
-                };
-
-                for i in 1..lines.len() {
-                    let split = lines[i].split(" = ");
-                    let config: Vec<String> = split.map(|s| s.to_string()).collect();
-                    let key = &config[0];
-                    let value = config[1].to_owned();
-                    match key.as_ref() {
-                        "mfa_serial" => aws_profile.mfa_serial = Some(value),
-                        "output" => aws_profile.output = Some(value),
-                        "region" => aws_profile.region = Some(value),
-                        "role_arn" => aws_profile.role_arn = Some(value),
-                        "source_profile" => aws_profile.source_profile = Some(value),
-                        "ot_session_name" => aws_profile.ot_session_name = Some(value),
-                        "ot_expiration" => aws_profile.ot_expiration = Some(value),
-                        "ot_source_profile" => aws_profile.ot_source_profile = Some(value),
-                        "ot_role_arn" => aws_profile.ot_role_arn = Some(value),
-                        _ => (),
-                    }
-                }
-
-                if profile_name != "" {
-                    aws_config.profiles.insert(profile_name.clone(), aws_profile);
-                }
-            }
-
-            self.AWSConfig = aws_config;
-        }
-
-        pub fn create(&self, profile_name: &str) {
-            println!("Creating session for profile \"{}\"...", profile_name);
-            let aws_profile = &self.AWSConfig.get_profile(profile_name);
-
-            let mut provider = ProfileProvider::new().unwrap();
-            provider.set_profile(profile_name);
-            // let region = &aws_profile.region;
-            // match region {
-            //     &Some(ref region) => region,
-            //     &None => panic!("You must specify a region for profile {}", profile_name)
-            // };
-            // println!("Region: {:?}", region.as_ref().unwrap());
-            let client = StsClient::new(default_tls_client().unwrap(), provider, Region::EuWest1);
-
-            let mfa_serial = &aws_profile.mfa_serial;
-            if mfa_serial.is_some() {
-                print!("Enter AWS MFA code for profile [{}]: ", profile_name);
-                stdout().flush();
-                let mut token_code = String::new();
-                stdin().read_line(&mut token_code)
-                    .ok()
-                    .expect("Couldn't read line");    
-                token_code.pop(); // Remove newline
-
-                let request = GetSessionTokenRequest {
-                    duration_seconds: None,
-                    serial_number: Some(mfa_serial.as_ref().unwrap().to_owned()),
-                    token_code: Some(token_code),
-                };
-                let response = client.get_session_token(&request);
-                match response {
-                    Ok(response) => {
-                        match save_profile(profile_name, &aws_profile) {
-                            Ok(_) => println!("Saved profile to config file."),
-                            Err(err) => println!("Error saving profile config to file: {:?}", err)
-                        };
-                        let credentials = response.credentials.unwrap();
-                        match save_credentials(profile_name, credentials) {
-                            Ok(_) => println!("Saved to credentials file."),
-                            Err(err) => println!("Error saving credentials to file: {:?}", err)
-                        };
-                    },
-                    Err(err) => panic!("Failed to get session token for profile {}: {:?}", profile_name, err),
-                };
-            }
-        }
-
-        pub fn show(&self, profile_name: &str) {
-            println!("Showing config for profile {}...", profile_name);
-            println!("{:?}", self.AWSConfig.profiles[profile_name]);
-        }
-    }
-
     pub fn create(profile_name: &str) {
+        println!("Loading config file");
+        let aws_config = load_config();
         println!("Creating session for profile \"{}\"...", profile_name);
+        let aws_profile = aws_config.get_profile(profile_name);
 
+        let mut provider = ProfileProvider::new().unwrap();
+        provider.set_profile(profile_name);
+        // let region = &aws_profile.region;
+        // match region {
+        //     &Some(ref region) => region,
+        //     &None => panic!("You must specify a region for profile {}", profile_name)
+        // };
+        // println!("Region: {:?}", region.as_ref().unwrap());
+        let client = StsClient::new(default_tls_client().unwrap(), provider, Region::EuWest1);
+
+        let mfa_serial = &aws_profile.mfa_serial;
+        if mfa_serial.is_some() {
+            print!("Enter AWS MFA code for profile [{}]: ", profile_name);
+            stdout().flush();
+            let mut token_code = String::new();
+            stdin().read_line(&mut token_code)
+                .ok()
+                .expect("Couldn't read line");    
+            token_code.pop(); // Remove newline
+
+            let request = GetSessionTokenRequest {
+                duration_seconds: None,
+                serial_number: Some(mfa_serial.as_ref().unwrap().to_owned()),
+                token_code: Some(token_code),
+            };
+            let response = client.get_session_token(&request);
+            match response {
+                Ok(response) => {
+                    match save_profile(profile_name, &aws_profile) {
+                        Ok(_) => println!("Saved profile to config file."),
+                        Err(err) => println!("Error saving profile config to file: {:?}", err)
+                    };
+                    let credentials = response.credentials.unwrap();
+                    match save_credentials(profile_name, credentials) {
+                        Ok(_) => println!("Saved to credentials file."),
+                        Err(err) => println!("Error saving credentials to file: {:?}", err)
+                    };
+                },
+                Err(err) => panic!("Failed to get session token for profile {}: {:?}", profile_name, err),
+            };
+        }
     }
-
-    pub fn new() -> SessionHandler {
-            SessionHandler {
-                AWSConfig: AWSConfig {
-                    profiles: HashMap::new()
-                }
-            }
+    pub fn show(profile_name: &str) {
+        println!("Showing config for profile {}...", profile_name);
     }
 
     pub fn refresh() {
@@ -185,6 +96,76 @@ pub mod SessionHandler {
 
     pub fn clean() {
         println!("Cleaning sessions...");
+    }
+
+    fn load_config() -> AWSConfig {
+        let aws_config_file = read_aws_config_file();
+        let profiles = split_config_file(aws_config_file);
+
+        // Create an instance of the AWSConfig struct
+        let mut aws_config = AWSConfig {
+            profiles: HashMap::new()
+        };
+
+        // Iterate over profile chunks
+        for profile in profiles {
+            // Create a vector of string that holds each line
+            let split = profile.split("\n");
+            let lines: Vec<String> = split.map(|s| s.to_string()).collect();
+
+            // Get the profile name from the first line
+            let profile_line = lines[0].to_owned();
+
+            // Lets ignore the default for now
+            if profile_line == "[default]" {
+                continue;
+            }
+
+            let split2 = profile_line.split(" ");
+            let mut words: Vec<String> = split2.map(|s| s.to_string()).collect();
+            let mut profile_name = String::new();
+            if words.len() > 1 {
+                words[1].pop();
+                profile_name = words[1].trim_right().to_owned();
+            }
+
+            let mut aws_profile = AWSProfile{
+                source_profile: None,
+                region: None,
+                output: None,
+                role_arn: None,
+                mfa_serial: None,
+                ot_expiration: None,
+                ot_session_name: None,
+                ot_source_profile: None,
+                ot_role_arn: None,
+            };
+
+            for i in 1..lines.len() {
+                let split = lines[i].split(" = ");
+                let config: Vec<String> = split.map(|s| s.to_string()).collect();
+                let key = &config[0];
+                let value = config[1].to_owned();
+                match key.as_ref() {
+                    "mfa_serial" => aws_profile.mfa_serial = Some(value),
+                    "output" => aws_profile.output = Some(value),
+                    "region" => aws_profile.region = Some(value),
+                    "role_arn" => aws_profile.role_arn = Some(value),
+                    "source_profile" => aws_profile.source_profile = Some(value),
+                    "ot_session_name" => aws_profile.ot_session_name = Some(value),
+                    "ot_expiration" => aws_profile.ot_expiration = Some(value),
+                    "ot_source_profile" => aws_profile.ot_source_profile = Some(value),
+                    "ot_role_arn" => aws_profile.ot_role_arn = Some(value),
+                    _ => (),
+                }
+            }
+
+            if profile_name != "" {
+                aws_config.profiles.insert(profile_name.clone(), aws_profile);
+            }
+        }
+
+        aws_config
     }
 
     fn read_aws_config_file() -> String {
--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,15 @@ use bluenine::SessionHandler;
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-    let mut session_handler = SessionHandler::new();
-    session_handler.load_config();
-    println!("Successfully loaded config!");
 
     match matches.subcommand() {
         ("create", Some(sub_m)) => {
             let profile_name = sub_m.value_of("profile_name").unwrap();
-            session_handler.create(profile_name);
+            SessionHandler::create(profile_name);
         },
         ("show", Some(sub_m)) => {
             let profile_name = sub_m.value_of("profile_name").unwrap();
-            session_handler.show(profile_name);
+            SessionHandler::show(profile_name);
         },
         ("refresh", _) => {
             SessionHandler::refresh()
